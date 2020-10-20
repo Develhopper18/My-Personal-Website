7f5639fc697d70a9ef5917101cc0cee6bfebac1c
Develhopper18 <team18developers@gmail.com>
2020-10-20 18:37:23 +0530

    My Commit dvlhpr18

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggMn() {
+    const toggMn = document.querySelector(".toggMn");
+    const navigation = document.querySelector(".navigation");
+    toggMn.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
+function btn() {
+    const btn = document.querySelector(".btn");
+    let btt = document.getElementById("btn")
+    btn.classList.toggle("active")
+}
